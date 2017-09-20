a0c42b013ca6035194c449ff7a315b317c5cf219
wh1te5now <mister_saize@mail.ru>
2017-09-20 09:03:56 +0300

    initial

---
+++ Article.php
@@ -0,0 +1,38 @@
+<?php
+
+    class Article
+    {
+
+        private $_id;
+        private $_title;
+        private $_previewText;
+        private $_fullText;
+        private $_author;
+        private $_created_at;
+        private $_updated_at;
+        private $_published_at;
+        private $_expired_at;
+        private $_editor;
+        private $_image;
+
+        public function getByCategory() {
+
+        }
+
+        public function create() {
+
+        }
+
+        public function getAll() {
+
+        }
+
+        public function update() {
+
+        }
+
+        public function delete() {
+
+        }
+
+    }
---
+++ Category.php
@@ -0,0 +1,24 @@
+<?php
+
+    class Category
+    {
+        private $_id;
+        private $_name;
+        private $_parent;
+
+        public function getAll() {
+
+        }
+
+        public function create() {
+
+        }
+
+        public function update() {
+
+        }
+
+        public function delete() {
+
+        }
+    }
